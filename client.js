ea996fd47b4734fd4ebc0fd232f528121758ff1f
Scrabakos <scrab4ever@hotmail.com>
2023-12-27 20:25:07 +0200

    sync

--- client.js
+++ client.js
@@ -26,6 +26,197 @@ const dateTime = () => {
 }
 
 
+/*
+ 
+            await connector.REFUND_EXPIRED();
+       
+            gsBalance = await connector.GET_GAMESERVER_BALANCE();
+ 
+            console.log(dateTime() + " | Gameserver balance: " + gsBalance);
+ 
+            setInterval(async () => {
+ 
+                const prevBalance = gsBalance;
+ 
+                await connector.UPDATE_LOGINSERVER_DATA(gsID);
+ 
+                gsBalance = await connector.GET_GAMESERVER_BALANCE();
+ 
+                const delta = Number(gsBalance) - Number(prevBalance);
+ 
+                if (delta != 0)
+                    console.log(dateTime() + " | Gameserver balance : " + gsBalance + " (" + prevBalance + String((delta > 0) ? ("+" + delta) : (delta)) + "=" + gsBalance + ")");
+            }, 2500);
+ 
+            console.log(dateTime() + " | Connecting to service...");
+
+                    wsClient = io("wss://donation.fiskpay.com:42099", { "autoConnect": false, "reconnection": true, "reconnectionDelay": 5000, "reconnectionAttempts": Infinity });
+//wsClient = io("ws://127.0.0.1:42099", { "autoConnect": false, "reconnection": true, "reconnectionDelay": 5000, "reconnectionAttempts": Infinity });
+wsClient.on("connect", () => {
+
+    wsClient.once("connect_error", () => {
+
+        console.log(dateTime() + " | Client will reconnect automatically");
+        console.log(dateTime() + " | Do not close the client");
+    });
+
+    console.log(dateTime() + " | Connection to service established");
+    wsClient.emit("clientRequest", connector.GET_ADDRESS, connector.GET_HASH, tokenSymbol, gsID);
+}).on("disconnect", () => {
+
+    console.log(dateTime() + " | Connection to service dropped");
+}).on("accepted", () => {
+
+    console.log(dateTime() + " | Signed in as " + config.SignIn.ethereumAddress);
+}).on("failed", () => {
+
+    console.log(dateTime() + " | Ethereum address, password, login connector remote IP address, login connector port combination mismatch");
+    process.exit();
+}).on("duplicateGS", () => {
+
+    console.log(dateTime() + " | Service already connected to gameserver with \"server_id\" = " + gsID);
+    process.exit()
+}).on("timeout", () => {
+
+    console.log(dateTime() + " | Timeout received. Reopen your client");
+    process.exit()
+}).on("msg", async (obj) => {
+
+    if (obj.type == "req") {
+
+        if (obj.step % 2 != 1) { //We received a request (req) that has an even numbered step (i.e. 2,4...).>>>>>>>         >>>>>>>         >>>>>>>         >>>>>>>         THIS MUST NEVER HAPPEN
+
+            console.log(dateTime() + " | Received a even-step message\nMessage: " + JSON.stringify(obj));
+        }
+        else { //We received a new request (req) from a wsClient or the wsServer.>>         >>>>>>>         >>>>>>>         >>>>>>>         >>>>>>>         >>>>>>>         MAY OR MAY NOT EMIT a response (res)
+
+            let emit = false;
+            let res = JSON.parse(JSON.stringify(obj));
+            res.type = "res";
+            res.data = {};
+
+            if (obj.subject == "getAccs" && obj.step == 1) {
+
+                emit = true;
+                res.data.accounts = await connector.GET_ACCOUNTS(obj.data.ethAddress);
+            }
+            else if (obj.subject == "addAcc" && obj.step == 1) {
+
+                emit = true;
+
+                if (await connector.VERIFY_PASSWORD(obj.data.username, obj.data.password))
+                    res.data.added = await connector.ADD_ADDRESS(obj.data.username, obj.data.ethAddress);
+                else
+                    res.data.added = false;
+            }
+            else if (obj.subject == "removeAcc" && obj.step == 1) {
+
+                emit = true;
+
+                if (await connector.VERIFY_PASSWORD(obj.data.username, obj.data.password))
+                    res.data.removed = await connector.REMOVE_ADDRESS(obj.data.username, obj.data.ethAddress);
+                else
+                    res.data.removed = false;
+            }
+            else if (obj.subject == "getChars" && obj.step == 1) {
+
+                emit = true;
+                res.data.characters = await connector.GET_CHARACTERS(obj.data.username);
+            }
+            else if (obj.subject == "getCharBalance" && obj.step == 1) {
+
+                emit = true;
+                res.data.balance = await connector.GET_CHARACTER_BALANCE(obj.data.character);
+            }
+            else if (obj.subject == "newWithdraw" && obj.step == 1) {
+
+                const trueAmount = await connector.GET_CHARACTER_BALANCE(obj.data.character);
+
+                if (trueAmount === false || obj.data.amount > trueAmount) {
+
+                    emit = true;
+                    res.data.failed = true;
+                    res.data.failReason = "Character insufficient balance";
+                }
+                else {
+
+                    let req = JSON.parse(JSON.stringify(msgObject));
+
+                    req.step = 1;
+                    req.subject = "newWithdraw";
+                    req.type = "req";
+                    req.from = obj.to;
+                    req.to = "srv";
+                    req.data.panel = obj.from;
+                    req.data.from = obj.data.from;
+                    req.data.to = obj.data.walletAddress;
+                    req.data.symbol = tokenSymbol;
+                    req.data.amount = obj.data.amount;
+                    req.data.server = gsID;
+                    req.data.character = obj.data.character;
+                    req.data.signedMessage = obj.data.signedMessage;
+                    req.data.refund = await connector.ADD_REFUND_AND_DECREASE_BALANCE(gsID, obj.data.character, obj.data.amount, obj.data.walletAddress);
+
+                    if (req.data.refund !== false)
+                        wsClient.emit("msg", req);
+                }
+            }
+            else if (obj.subject == "revertWithdraw" && obj.step == 1) {
+
+                if (await connector.REMOVE_REFUND(gsID, obj.data.character, obj.data.amount, obj.data.refund) !== false)
+                    if (await connector.INCREASE_BALANCE(obj.data.character, obj.data.amount) === false)
+                        console.log(dateTime() + " | You must reward character " + obj.data.character + " with " + obj.data.amount + " tokens ingame. Automatic token refund failed");
+            }
+            else if (obj.subject == "logDeposit" && obj.step == 1) {
+
+                if (await connector.INCREASE_BALANCE(obj.data.character, obj.data.amount) === false)
+                    console.log(dateTime() + " | You must reward character " + obj.data.character + " with " + obj.data.amount + " tokens ingame. Automatic token increase failed");
+                else if (await connector.LOG_DEPOSIT(obj.data.txHash, gsID, obj.data.character, obj.data.from, obj.data.symbol, obj.data.amount) !== false)
+                    console.log(dateTime() + " | Deposit from address " + obj.data.from + " to character " + obj.data.character + " (" + obj.data.amount + " " + obj.data.symbol + ")");
+            }
+            else if (obj.subject == "logWithdrawal" && obj.step == 1) {
+
+                if (await connector.REMOVE_REFUND(gsID, obj.data.character, obj.data.amount, obj.data.refund) === false)
+                    console.log(dateTime() + " | You must remove " + obj.data.amount + " tokens from character " + obj.data.character + " ingame. Automatic token decrease failed");
+                else if (await connector.LOG_WITHDRAWAL(obj.data.txHash, gsID, obj.data.character, obj.data.to, obj.data.symbol, obj.data.amount) !== false)
+                    console.log(dateTime() + " | Withdrawal from character " + obj.data.character + " to address " + obj.data.to + " (" + obj.data.amount + " " + obj.data.symbol + ")");
+            }
+            else if (obj.subject == "panelUpdate" && obj.step == 1) {
+
+                if (obj.data.disconnect)
+                    console.log(dateTime() + " | Panel disconnected (" + obj.data.panelID + ")");
+                else
+                    console.log(dateTime() + " | Panel connected (" + obj.data.panelID + ")");
+            }
+            else if (obj.subject == "getGsBalance" && obj.step == 1) {
+
+                emit = true;
+                res.data.balance = gsBalance;
+            }
+            else if (obj.subject == "getLsData" && obj.step == 1) {
+
+                emit = true;
+
+                const tmp = await connector.GET_LOGINSERVER_DATA(gsID);
+                res.data.balance = tmp.balance;
+                res.data.nChars = tmp.nChars;
+            }
+
+            if (emit) {
+
+                res.step++;
+                wsClient.emit("msg", res);
+            }
+        }
+
+    }
+}).on("error", (e) => { console.log(e) });
+            wsClient.connect();
+            
+}
+*/
+
+
 const client = async () => {
 
     console.log(dateTime() + " | -----------------------------------------------------------------------------------");
@@ -55,7 +246,6 @@ const client = async () => {
         const msgObject = { "step": null, "subject": null, "type": null, "from": null, "to": null, "data": {} };
         const tokenSymbol = "LINK";
         const socketConnector = io("ws://127.0.0.1:42099", { "autoConnect": false, "reconnection": true, "reconnectionDelay": 5000, "reconnectionAttempts": Infinity }); // "wss://donation.fiskpay.com:42099" "ws://127.0.0.1:42099"
-        let socketStatus = false;
 
         serverConnector.on("update", async (id, connected) => {
 
@@ -78,9 +268,9 @@ const client = async () => {
                     console.log(dateTime() + " | Server `" + id + "` database connection failed");
             }
 
-            socketConnector()
-
             serverStatus[id].c = connected;
+
+            socketConnector.volatile.emit("serverStatus", serverStatus);
         });
 
         console.log(dateTime() + " |");
@@ -88,54 +278,71 @@ const client = async () => {
 
         let cfg = connectorConfig["ls"];
 
-        if (cfg && cfg.dbName && cfg.dbPort && cfg.dbUsername && cfg.dbPassword && cfg.dbTableColumns && cfg.dbTableColumns.accounts && cfg.dbTableColumns.gameservers) {
+        if (!(cfg && cfg.dbName && cfg.dbPort && cfg.dbUsername && cfg.dbPassword && cfg.dbTableColumns && cfg.dbTableColumns.accounts && cfg.dbTableColumns.gameservers)) {
+
+            console.log(dateTime() + " | Server `ls` improper configuration");
+            process.end();
+        }
+
+        await new Promise(async (resolve) => {
 
             serverStatus["ls"] = {};
-
             await serverConnector.CONNECT_SERVER("ls");
-            await new Promise((resolve) => {
+
+            const interval = setInterval(() => {
+
+                if (serverStatus["ls"].v !== undefined) {
+
+                    clearInterval(interval);
+
+                    if (serverStatus["ls"].v !== true)
+                        process.end();
+
+                    resolve(true);
+                }
+            }, 250);
+        });
+
+        console.log(dateTime() + " |");
+        console.log(dateTime() + " | Connecting to gameserver(s) database...");
+
+        for (const id of await serverConnector.GET_IDS()) {
+
+            if ((!(/^\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-7])\b$/).test(id)))
+                console.log(dateTime() + " | Server `" + id + "` using improper ID value (skipped)");
+            else if (!(serverStatus[id] === undefined))
+                console.log(dateTime() + " | Server `" + id + "` using duplicate ID value (skipped)");
+
+            cfg = connectorConfig[id];
+
+            if (!(cfg && cfg.rewardId && cfg.dbName && cfg.dbIPAddress && cfg.dbPort && cfg.dbUsername && cfg.dbPassword && cfg.dbTableColumns && cfg.dbTableColumns.characters && cfg.dbTableColumns.items)) {
+
+                console.log(dateTime() + " | Server `" + id + "` improper configuration");
+                process.end();
+            }
+
+            await new Promise(async (resolve) => {
+
+                serverStatus[id] = {};
+                await serverConnector.CONNECT_SERVER(id);
 
                 const interval = setInterval(() => {
 
-                    if (serverStatus["ls"].v !== undefined && serverStatus["ls"].c !== undefined) {
+                    if (serverStatus[id].v !== undefined) {
 
                         clearInterval(interval);
+
+                        if (serverStatus[id].v !== true)
+                            process.end();
+
                         resolve(true);
                     }
                 }, 250);
             });
-
-            console.log(dateTime() + " |");
-            console.log(dateTime() + " | Connecting to gameserver(s) database...");
-
-            if (serverStatus["ls"].v === true && serverStatus["ls"].c === true) {
-
-                for (const id of await serverConnector.GET_IDS()) {
-
-                    if (((/^\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-7])\b$/).test(id) && serverStatus[id] === undefined)) {
-
-                        cfg = connectorConfig[id];
-
-                        if (cfg && cfg.rewardId && cfg.dbName && cfg.dbIPAddress && cfg.dbPort && cfg.dbUsername && cfg.dbPassword && cfg.dbTableColumns && cfg.dbTableColumns.characters && cfg.dbTableColumns.items) {
-
-                            serverStatus[id] = {};
-                            await serverConnector.CONNECT_SERVER(id);
-                        }
-                    }
-                    else
-                        console.log(dateTime() + " | Server `" + id + "` improper configuration (ignored)");
-                };
-            }
-        }
-        else {
-
-            console.log(dateTime() + " | Server `ls` improper configuration (fail)");
-            process.end();
         }
 
         console.log(dateTime() + " |");
-        console.log(dateTime() + " | Connecting to service...");
-
+        console.log(dateTime() + " | Connecting to fiskpay service...");
 
     }
     catch (error) {
